6bb7a770682edb3a40d132366fd4f41557e9c9cd
sangwondee <sangwondee@gmail.com>
2019-05-27 11:57:36 +0700

    first test ok yubbbb

---
+++ tests/Feature/BookReservationTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Book;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class BookReservationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_book_can_be_added_to_the_library()
+    {
+        $this->withoutExceptionHandling();
+
+        $response = $this->post('/books', [
+            'title' => 'Cool Book Title',
+            'author' => 'Sangwondee'
+        ]);
+
+        $response->assertOk();
+
+        $this->assertCount(1, Book::all());
+    }
+}

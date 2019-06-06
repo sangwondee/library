83bbbb47d0f39f116b117bd6e9962df8ced43e8b
sangwondee <sangwondee@gmail.com>
2019-06-06 17:34:44 +0700

    change welcome and web route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel test commit
+                    Library
                 </div>
 
                 <div class="links">

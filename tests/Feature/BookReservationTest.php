9416c3adaaab310dc7abd0ea71c31b7e7c14ad2a
sangwondee <sangwondee@gmail.com>
2019-06-08 23:21:20 +0700

    delete book reservationtest

--- tests/Feature/BookReservationTest.php
+++ tests/Feature/BookReservationTest.php
@@ -13,8 +13,6 @@ class BookReservationTest extends TestCase
     /** @test */
     public function a_book_can_be_added_to_the_library()
     {
-        $this->withoutExceptionHandling();
-
         $response = $this->post('/books', [
             'title' => 'Cool Book Title',
             'author' => 'Sangwondee'
